ddb86ee9b054f2d4f7f6cdfaee55489af90edfdd
Arsen Melkumiants <arsen.mem@gmail.com>
2017-10-22 14:19:49 +0200

    Fixes Socket port

--- src/client/app/utils/socket.js
+++ src/client/app/utils/socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client';
 
 class Socket {
 	static connect() {
-		let socketHost = `${window.location.protocol}//${window.location.hostname}:3001`;
+		let socketHost = `${window.location.protocol}//${window.location.hostname}:8888`;
 
 		return io.connect(socketHost);
 	}
